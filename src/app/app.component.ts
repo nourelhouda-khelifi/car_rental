3dc5814739ab1573ebec867cb4651be81aea83ba
nour khalifi <nourkhalifi74@gmail.com>
2024-12-04 18:06:12 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrected from 'styleUrl' to 'styleUrls'
 })
 export class AppComponent {
   theme: string = 'light';
 
   toggleTheme() {
-    this.theme = this.theme === 'dark' ? 'light' : 'dark'; // Inverse le thème actuel
+    this.theme = this.theme === 'dark' ? 'light' : 'dark';
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,36 +3,27 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'] // Corrected from 'styleUrl' to 'styleUrls'
 })
-
 export class NavbarComponent {
-   // Déclaration d'une variable d'entrée qui est liée au thème (mode clair ou sombre)
-   @Input() theme: string = 'light'; 
-   // Événement émis lorsque le thème change (par exemple, lorsqu'on passe du mode sombre au mode clair)
-   @Output() themeChange = new EventEmitter<string>(); 
- 
-   // Variable pour contrôler l'affichage du menu mobile
-   showMenu = false;
- 
-   // Liste des liens de navigation dans la Navbar
-   navLinks = [
-     { id: 1, name: 'HOME', link: '/#' },
-     { id: 2, name: 'CARS', link: '/#cars' },
-     { id: 3, name: 'ABOUT', link: '/#about' },
-     { id: 4, name: 'BOOKING', link: '/#booking' }
-   ];
- 
-   // Fonction pour basculer entre l'affichage du menu mobile
-   toggleMenu() {
-     this.showMenu = !this.showMenu;
-   }
- 
-   // Fonction pour basculer entre le mode sombre et clair
-   toggleTheme() {
-     const newTheme = this.theme === 'dark' ? 'light' : 'dark'; // Inverse le thème actuel
-     this.themeChange.emit(newTheme); // Émet l'événement pour changer le thème
-   }
+  @Input() theme: string = 'light'; 
+  @Output() themeChange = new EventEmitter<string>();
 
+  showMenu = false;
 
+  navLinks = [
+    { id: 1, name: 'HOME', link: '/#' },
+    { id: 2, name: 'CARS', link: '/#cars' },
+    { id: 3, name: 'ABOUT', link: '/#about' },
+    { id: 4, name: 'BOOKING', link: '/#booking' }
+  ];
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  toggleTheme() {
+    const newTheme = this.theme === 'dark' ? 'light' : 'dark';
+    this.themeChange.emit(newTheme);
+  }
 }
